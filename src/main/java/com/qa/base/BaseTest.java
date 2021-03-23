7bff35a4fddda62953e80712762e72994487254b
akashpatil8494 <akashpatil8494@gmail.com>
2021-03-23 23:09:00 +0530

    create new reop

---
+++ src/main/java/com/qa/base/BaseTest.java
@@ -0,0 +1,45 @@
+package com.qa.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+	public static FileInputStream fs;
+	public static Properties prop;
+	public static WebDriver driver;
+
+	public BaseTest() throws IOException {
+		prop = new Properties();
+		try {
+			fs = new FileInputStream(
+					"C:\\Users\\TOSHIBA\\eclipse-workspace\\new\\src\\main\\java\\com\\qa\\config\\config.properties");
+
+			prop.load(fs);
+
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+	public static void setUp() {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\TOSHIBA\\eclipse-workspace\\new\\src\\main\\java\\com\\qa\\driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.get("");
+
+	}
+}
